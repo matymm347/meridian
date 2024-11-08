5b62c9234ccbcb811d3e29e94764cf362c74bdf5
matymm347 <mateuszjochimiak@gmail.com>
2024-11-08 12:44:25 +0100

    Slider updated with proper value labels and different "darks" marked on rail

--- src/components/HoursSlider.jsx
+++ src/components/HoursSlider.jsx
@@ -1,30 +1,79 @@
+import PropTypes from "prop-types";
 import Box from "@mui/material/Box";
 import Slider from "@mui/material/Slider";
 import { useState } from "react";
 
-function valuetext(value) {
-  return `${value}°C`;
-}
-
-export default function HoursSlider() {
-  const [value, setValue] = useState([10, 13]);
+export default function HoursSlider({
+  astroMidnight = "01:15",
+  astroDarkLength = "02:30",
+  nauticalDarkLength = "08:15",
+  civilDarkLength = "12:20",
+}) {
+  const [value, setValue] = useState([500, 1000]);
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
+
+  function timeToMinutes(timeStr) {
+    const [hours, minutes] = timeStr.split(":").map(Number);
+    return hours * 60 + minutes;
+  }
+
+  const valueLabelFormat = (value) => {
+    // center on astroMidnight
+    value = value + 720 + timeToMinutes(astroMidnight);
+    if (value > 1440) {
+      value -= 1440;
+    }
+
+    const hours = Math.floor(value / 60);
+    let minutes = value % 60;
+    // add leading 0
+    if (minutes < 10) {
+      minutes = `0${minutes}`;
+    }
+
+    return `${hours}:${minutes}`;
+  };
+
   return (
     <>
-      <Box sx={{ width: 300 }}>
+      <Box sx={{ width: 600 }}>
         <Slider
           getAriaLabel={() => "Observation hours"}
           value={value}
           onChange={handleChange}
-          valueLabelDisplay="auto"
-          getAriaValueText={valuetext}
+          valueLabelDisplay="on"
           min={0}
-          max={24}
+          max={1440}
+          valueLabelFormat={valueLabelFormat}
+          sx={{
+            "& .MuiSlider-rail": {
+              background: `radial-gradient(
+              circle, 
+                navy 0%,
+                navy ${timeToMinutes(astroDarkLength) / 14.4}%,
+                blue ${timeToMinutes(astroDarkLength) / 14.4}%,
+                blue ${timeToMinutes(nauticalDarkLength) / 14.4}%,
+                azure ${timeToMinutes(nauticalDarkLength) / 14.4}%,
+                azure ${timeToMinutes(civilDarkLength) / 14.4}%,
+                cadetblue ${timeToMinutes(civilDarkLength) / 14.4}%,
+                cadetblue 100%
+              )`, // Gradient for rail colors
+              opacity: 1, // Ensure the rail is fully opaque
+              height: 20,
+            },
+          }}
         />
       </Box>
     </>
   );
 }
+
+HoursSlider.propTypes = {
+  astroMidnight: PropTypes.string,
+  astroDarkLength: PropTypes.string,
+  nauticalDarkLength: PropTypes.string,
+  civilDarkLength: PropTypes.string,
+};
