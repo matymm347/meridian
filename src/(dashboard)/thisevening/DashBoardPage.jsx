ae7fdd076027fe3102ffc649f1eb6370b4942c61
matymm347 <mateuszjochimiak@gmail.com>
2024-11-18 16:42:58 +0100

    Developing useState for location api request and response

--- src/(dashboard)/thisevening/DashBoardPage.jsx
+++ src/(dashboard)/thisevening/DashBoardPage.jsx
@@ -1,13 +1,37 @@
-import Box from "@mui/material/Box";
+import { Autocomplete, Box } from "@mui/material";
 import TextField from "@mui/material/TextField";
 import HoursSlider from "../../components/HoursSlider";
+import { useEffect, useState } from "react";
 
 export default function DashboardPage() {
+  const [placeName, setPlaceName] = useState("");
+  const [placesList, setPlacesList] = useState([]);
+
+  useEffect(() => {
+    if (placeName === "") {
+      setPlacesList([]);
+      return;
+    }
+
+    setTimeout(() => {
+      fetch(`http://localhost:3000/geocode?placename=${placeName}`).then(
+        (response) => response.json()
+      ); // some function to set placesList here
+    }, 500);
+  });
+
   return (
     <>
       <Box>
         <p>Your location:</p>
-        <TextField id="location-name" label="Location name" variant="filled" />
+        <Autocomplete
+          sx={{ width: 300 }}
+          filterOptions={(x) => x} // disable built-in filtering
+          disablePortal
+          options={placesList}
+          renderInput={(params) => <TextField {...params} label="Location" />}
+        />
+        <br />
         <TextField id="longitude" label="Longitude" variant="filled" />
         <TextField id="latitude" label="Latitude" variant="filled" />
 
