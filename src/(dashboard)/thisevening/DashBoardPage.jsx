65d3ecac665e23b9127b8236a63d24a05847ca6a
matymm347 <mateuszjochimiak@gmail.com>
2024-11-20 22:36:14 +0100

    Longitude and latitude now using api data

--- src/(dashboard)/thisevening/DashBoardPage.jsx
+++ src/(dashboard)/thisevening/DashBoardPage.jsx
@@ -4,9 +4,21 @@ import HoursSlider from "../../components/HoursSlider";
 import { useEffect, useState } from "react";
 
 export default function DashboardPage() {
+  const [apiData, setApiData] = useState({});
   const [placeName, setPlaceName] = useState("");
+  const [selectedPlaceId, setSelectedPlaceId] = useState(null);
+  const [longitude, setLongitude] = useState("Longitude");
+  const [latitude, setLatitude] = useState("Latitude");
   const [placesList, setPlacesList] = useState([]);
   const [delayActive, setDelayActive] = useState(false);
+
+  function handlePlaceChange(value) {
+    const id = placesList.indexOf(value);
+    setSelectedPlaceId(id);
+    console.log(id);
+    setLongitude(apiData.features[id].geometry.coordinates[0]);
+    setLatitude(apiData.features[id].geometry.coordinates[1]);
+  }
 
   useEffect(() => {
     if (placeName === "") {
@@ -20,8 +32,9 @@ export default function DashboardPage() {
         fetch(`http://localhost:3000/geocode?placename=${placeName}`).then(
           (response) =>
             response.json().then((data) => {
+              setApiData(data);
               let places = data.features.map((feature) => {
-                return feature.place_name;
+                return `${feature.place_name}, ${feature.context[2].text}`;
               });
               setPlacesList(places);
             })
@@ -38,16 +51,17 @@ export default function DashboardPage() {
       <Box>
         <p>Your location:</p>
         <Autocomplete
+          onChange={(event, value) => handlePlaceChange(value)}
           onInputChange={(event, place) => setPlaceName(place)}
-          sx={{ width: 300 }}
+          sx={{ width: 600 }}
           filterOptions={(x) => x} // disable built-in filtering
           disablePortal
           options={placesList}
           renderInput={(params) => <TextField {...params} label="Location" />}
         />
         <br />
-        <TextField id="longitude" label="Longitude" variant="filled" />
-        <TextField id="latitude" label="Latitude" variant="filled" />
+        <TextField id="longitude" label={longitude} variant="filled" />
+        <TextField id="latitude" label={latitude} variant="filled" />
 
         <p>Observation hours:</p>
         <HoursSlider />
