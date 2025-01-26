b2ac0b9fa3c1e40f70e3b033052adda8b6408a15
matymm347 <mateuszjochimiak@gmail.com>
2025-01-26 20:49:27 +0100

    Added Vercel's Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router";
+import { Analytics } from "@vercel/analytics/next";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <RouterProvider router={router} future={{ v7_startTransition: true }} />
+    <Analytics />
   </StrictMode>
 );
